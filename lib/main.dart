dcbc5d165e61defede53e24f3623fd2d1bfa7ab5
om <dodiyanihar8460@gmail.com>
2023-02-28 16:21:36 +0530

    IMP note

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:qr_scan/scan.dart';
 
+/* if not run go to main.dart >>>>>
+Edit Configurations... >>>>
+additional run arg >>>>> p
+aste --no-sound-null-safety
+after save do run again
+*/
 void main()
 {
   runApp(MaterialApp(
